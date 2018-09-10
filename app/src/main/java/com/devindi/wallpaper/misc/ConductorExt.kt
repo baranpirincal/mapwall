d54152e0feca655831b4be66dd403726a6bd3c95
Andrew Mihin <andrew@devindi.com>
2018-09-10 21:36:00 +0300

    Add controls to set custom size. Add new preference storage model

--- app/src/main/java/com/devindi/wallpaper/misc/ConductorExt.kt
+++ app/src/main/java/com/devindi/wallpaper/misc/ConductorExt.kt
@@ -79,6 +79,21 @@ inline fun <reified T : ViewModel> LifecycleController.viewModel(
 /**
  * Lazy get a viewModel instance
  *
+ * @param key - ViewModel Factory key (if have several instances from same ViewModel)
+ * @param name - Koin BeanDefinition name (if have several ViewModel definition of the same type)
+ * @param parameters - parameters to pass to the BeanDefinition
+ */
+inline fun <reified T : ViewModel> FixedLifecycleController.viewModel(
+        key: String? = null,
+        name: String? = null,
+        noinline parameters: Parameters = { emptyMap() }
+): Lazy<T> {
+    return viewModelByClass(T::class, key, name, parameters)
+}
+
+/**
+ * Lazy get a viewModel instance
+ *
  * @param clazz - Class of the BeanDefinition to retrieve
  * @param key - ViewModel Factory key (if have several instances from same ViewModel)
  * @param name - Koin BeanDefinition name (if have several ViewModel definition of the same type)
@@ -89,6 +104,23 @@ fun <T : ViewModel> LifecycleController.viewModelByClass(
     key: String? = null,
     name: String? = null,
     parameters: Parameters = { emptyMap() }
+): Lazy<T> {
+    return lazy { getViewModelByClass(clazz, key, name, parameters) }
+}
+
+/**
+ * Lazy get a viewModel instance
+ *
+ * @param clazz - Class of the BeanDefinition to retrieve
+ * @param key - ViewModel Factory key (if have several instances from same ViewModel)
+ * @param name - Koin BeanDefinition name (if have several ViewModel definition of the same type)
+ * @param parameters - parameters to pass to the BeanDefinition
+ */
+fun <T : ViewModel> FixedLifecycleController.viewModelByClass(
+        clazz: KClass<T>,
+        key: String? = null,
+        name: String? = null,
+        parameters: Parameters = { emptyMap() }
 ): Lazy<T> {
     return lazy { getViewModelByClass(clazz, key, name, parameters) }
 }
@@ -120,6 +152,33 @@ fun <T : ViewModel> LifecycleController.getViewModelByClass(
     return if (key != null) viewModelProvider.get(
         key,
         clazz.java
+    ) else viewModelProvider.get(clazz.java)
+}
+
+/**
+ * Get a viewModel instance
+ *
+ * @param fromActivity - create it from Activity (default false) - not used if on Activity
+ * @param clazz - Class of the BeanDefinition to retrieve
+ * @param key - ViewModel Factory key (if have several instances from same ViewModel)
+ * @param name - Koin BeanDefinition name (if have several ViewModel definition of the same type)
+ * @param parameters - parameters to pass to the BeanDefinition
+ */
+fun <T : ViewModel> FixedLifecycleController.getViewModelByClass(
+        clazz: KClass<T>,
+        key: String? = null,
+        name: String? = null,
+        parameters: Parameters = { emptyMap() }
+): T {
+    CustomKoinFactory.apply {
+        this.parameters = parameters
+        this.name = name
+    }
+    Koin.logger.log("[ViewModel] get for Controller @ $this")
+    val viewModelProvider = ViewModelProvider(ViewModelStores.of(this.activity as FragmentActivity), CustomKoinFactory)
+    return if (key != null) viewModelProvider.get(
+            key,
+            clazz.java
     ) else viewModelProvider.get(clazz.java)
 }
 
